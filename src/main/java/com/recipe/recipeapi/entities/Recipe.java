b36c8ad90e698a75d974b46d58563cab9ae9a0ba
Emmanuel_Sewah <sewah2012@gmail.com>
2022-11-08 03:06:12 +0100

    refactor: :hammer: fix issues with updating recipe

--- src/main/java/com/recipe/recipeapi/entities/Recipe.java
+++ src/main/java/com/recipe/recipeapi/entities/Recipe.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "recipe", schema = "public")
 public class Recipe {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
